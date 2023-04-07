5c7a336667cc8911366f056cf88b311624dac482
Kazeem Quadri <quadrikazeem01@gmail.com>
2023-04-07 06:57:46 +0100

    update url

--- FinancialApplication/Helpers/ApiRoutes.cs
+++ FinancialApplication/Helpers/ApiRoutes.cs
@@ -11,7 +11,7 @@ public static class ApiRoutes
         public const string _resetPassword = "reset-password";
         public const string _confirmEmail = "confirm-email";
         public const string _resendConfirmationEmail = "resend-confirmation-email";
-        public const string _registerOrLoginWithGoogle = "register-or-login/google";
+        public const string _registerOrLoginWithGoogle = "external/google";
     }
 
     public static class CategoryRoutes
